eef32836467d0057dc19a9611254ed566c83f936
DevilsDomain <alaraerzincan@gmail.com>
2023-02-15 15:09:31 +0100

    stoybook build error fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vitest/config';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,10 +8,5 @@ export default defineConfig({
     // …
     environment: 'happy-dom',
     globals: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
   },
 });
