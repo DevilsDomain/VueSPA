94c13b21f7bd3ba3c117c0b350c99578ce118189
DevilsDomain <alaraerzincan@gmail.com>
2023-02-15 15:23:57 +0100

    removed build files

--- storybook-static/assets/config-df61610e.js
+++
@@ -1,8 +0,0 @@
-import { d as s, r as i, a as d } from './decorateStory-f21d1d8c.js';
-import './iframe-5917f10b.js';
-import './index-3121cc12.js';
-import './index-73814c06.js';
-import './vue.esm-bundler-f95a757f.js';
-var p = { framework: 'vue3' };
-export { s as applyDecorators, p as parameters, i as render, d as renderToDOM };
-//# sourceMappingURL=config-df61610e.js.map
